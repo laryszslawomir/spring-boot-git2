8acdf830aa7b752b7ad3f9d7f790e1aaffae5558
slarysz <slarysz@kamsoft.pl>
2020-10-05 21:31:35 +0200

    Initial commit

---
+++ src/main/java/pl/testy/git/SpringGitApplication.java
@@ -0,0 +1,13 @@
+package pl.testy.git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringGitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/testy/git/SpringGitApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.testy.git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
